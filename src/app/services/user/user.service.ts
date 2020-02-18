49e1ad464e430e7ffd86d052e749597df3befb0b
Kulbhushan saxena <kulbhushan.saxena09@gmail.com>
2020-02-18 16:36:00 +0530

    Login Functionality

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -8,12 +8,19 @@ import { User } from '../../model/user/user';
 export class UserService {
  
   private usersUrl: string;
+  private loginUrl: string;
  
   constructor(private http: HttpClient) {
     this.usersUrl = 'http://localhost:8080/register';
+    this.loginUrl = 'http://localhost:8080/login';
   }
  
   public save(user: User) {
     return this.http.post(this.usersUrl, user);
+  }
+
+  public login(user: User) {
+    const body = {username: user.username, password: user.password};
+    return this.http.post(this.loginUrl, body);
   }
 }
